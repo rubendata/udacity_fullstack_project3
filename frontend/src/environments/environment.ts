0fe4c7e84efb03a40db340bccaa2e48c9b1823d2
rubendata <info@rubensimon.de>
2021-02-06 17:51:26 +0100

    changed environment.ts settings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'udarub.eu.auth0.com', // the auth0 domain prefix
-    audience: 'image', // the audience set for the auth0 app
+    url: 'udarub.eu', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
     clientId: 'dqEIJ5ADYUvqej8u73d9q4pKwTpmp9v6', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
